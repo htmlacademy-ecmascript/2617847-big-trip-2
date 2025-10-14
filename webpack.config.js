50882d79152fa2cd7769462404c4dd4951745373
Anton <Soloma_ru@mail.ru>
2025-10-14 21:02:30 +0300

    Установка babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,24 @@ module.exports = {
     clean: true,
   },
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html',
